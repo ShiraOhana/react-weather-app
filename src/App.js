1e2f0fcb873f2a3b9805096f0bc8797286fd3514
ShiraOhana <Soshiraohana@gmail.com>
2022-03-14 14:15:52 +0200

    Url to GitHub

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       <header className="App-header">
         <h1>Weather App </h1>
         <Weather />
+        <p>
+          {" "}
+          This project is coded by Shira Ohana and it's open-sourced on{" "}
+          <a href="https://github.com/ShiraOhana/react-weather-app">
+            GitHub
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://agitated-thompson-12c3d6.netlify.app/">netlify.</a>{" "}
+        </p>
       </header>
     </div>
   );
