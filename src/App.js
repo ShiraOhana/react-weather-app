88421ea44020612beaf50a0f3e32cfd76d771552
ShiraOhana <Soshiraohana@gmail.com>
2022-03-17 13:52:31 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -6,9 +6,20 @@ function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <h1>Weather App </h1>
           <Weather />
         </header>
+        <footer>
+          <div>
+            This project was coded by Shira Ohana and it's{" "}
+            <a
+              href="https://github.com/ShiraOhana/react-weather-app"
+              targer="_blank"
+            >
+              {" "}
+              open-sourced on GitHub
+            </a>
+          </div>
+        </footer>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -6,16 +6,21 @@ export default function Weather() {
   return (
     <div className="Weather">
       <form>
-        <div className="row">
+        <div className="row mt-3">
           <div className="col-8">
             <input
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
@@ -25,13 +30,14 @@ export default function Weather() {
         <li>Wednesday 07:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="mostly cloudy"
           ></img>
-          6°C
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
